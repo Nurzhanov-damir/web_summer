6696d0a38b223bee0ca32d888bb3a587bd1e298b
Damir Nurzhanov <d_nurzhanov@kbtu.kz>
2026-06-08 14:37:07 +0500

    Update

--- catalog/views.py
+++ catalog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Yo Hello everyone! This is my book catalog")
+    return HttpResponse("Yo Hello everyone! This is my book catalog.")
