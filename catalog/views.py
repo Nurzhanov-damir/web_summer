1c5dc4f0725cb3cdb8f89f89c095b2e486cd4cb0
Damir Nurzhanov <d_nurzhanov@kbtu.kz>
2026-06-08 15:17:15 +0500

    update

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+ 
 def index(request):
-    return HttpResponse("Yo Hello everyone! This is my book catalog.")
+    books = [
+        {'title': 'Norwegian Wood', 'author': 'Haruki Murakami', 'year': 1987, 'rating': '4/5'},
+        {'title': 'Kafka on the Shore', 'author': 'Haruki Murakami', 'year': 2002, 'rating': '5/5'},
+        {'title': 'The Sound of Waves', 'author': 'Yukio Mishima', 'year': 1954, 'rating': '4.5/5'},
+    ]
+    return render(request, 'catalog/index.html', {'books': books})
